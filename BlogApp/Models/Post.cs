90a0ccd38748ece7c01b2df87657c53efb36f77d
chenyu-zheng <zcy9998@gmail.com>
2018-09-19 16:42:06 -0500

    Implement rich text for Post body

--- BlogApp/Models/Post.cs
+++ BlogApp/Models/Post.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace BlogApp.Models
 {
@@ -12,6 +13,7 @@ namespace BlogApp.Models
         public DateTimeOffset? Updated { get; set; }
         public string Title { get; set; }
         public string Slug { get; set; }
+        [AllowHtml]
         public string Body { get; set; }
         public string MediaUrl { get; set; }
         public bool Published { get; set; }

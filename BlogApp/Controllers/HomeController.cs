cc358dcad0e185dcabca19b1ebe9c5cdcceb38b7
chenyu-zheng <zcy9998@gmail.com>
2018-09-23 20:07:18 -0500

    Update layout & styles

--- BlogApp/Controllers/HomeController.cs
+++ BlogApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlogApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,11 @@ namespace BlogApp.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.Posts.ToList());
         }
 
         public ActionResult About()

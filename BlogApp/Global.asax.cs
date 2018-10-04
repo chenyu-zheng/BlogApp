9c25672ef4997b727ed5aa52afe1448d4c881aa6
chenyu-zheng <zcy9998@gmail.com>
2018-10-04 17:58:23 -0500

    Add migration code to Global.asax

--- BlogApp/Global.asax.cs
+++ BlogApp/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BlogApp.Migrations;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,10 @@ namespace BlogApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var configuration = new Configuration();
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
         }
     }
 }

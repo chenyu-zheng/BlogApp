75c1146d0c52a8ea166511ab165b2e4c15c7c94c
chenyu-zheng <zcy9998@gmail.com>
2018-10-05 08:56:19 -0500

    Add code to create Uploads folder

--- BlogApp/Global.asax.cs
+++ BlogApp/Global.asax.cs
@@ -19,9 +19,13 @@ namespace BlogApp
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
+            // Update database
             var configuration = new Configuration();
             var migrator = new DbMigrator(configuration);
             migrator.Update();
+
+            // Create Uploads folder
+            System.IO.Directory.CreateDirectory(Server.MapPath("~/Uploads"));
         }
     }
 }
